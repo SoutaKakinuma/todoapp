4fbbba02002f0db8c992427744c539d71e2b5a40
柿沼颯太 <s.kakinuma@g.dreamcareer.co.jp>
2022-04-14 12:58:53 +0900

    API(POST)完成

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,8 +1,9 @@
 var express = require("express");
 var router = express.Router();
 
-const tasks = require("../../src/tasks");
+const tasks = require("../../src/tasks.js");
 const items = require("../../src/items");
+
 
 /* 商品一覧を取得するルーティング */
 router.get("/items", function (req, res, next) {
@@ -16,10 +17,11 @@ router.get("/items/:id", function (req, res, next) {
   res.send(item);
 });
 
-/* タスク登録ルーティング*/
+/* タスク登録ルーティング */
 router.post("/tasks", async function (req, res, next) {
   const postTasks = await tasks.postTasks(req.body);
   res.send(postTasks);
 });
+
 
 module.exports = router;
--- src/tasks.js
+++ src/tasks.js
@@ -5,21 +5,31 @@ const config = require("../config.js");
 
 //タスク新規登録 
 
+
 postTasks = async function (body) {
     let connection = null;
     try {
         connection = await mysql.createConnection(config.dbSetting);
         //SQL記述
         const sql =
-            "INSERT INTO todoapp.t-task (task_name, deadline, category_id) VALUES (?, ?, ?);";
+            "INSERT INTO todoapp.t_task (task_name, deadline, category_id) VALUES (?, ?, ?);";
         let d = [body.taskName, body.deadline, body.category];
         const [rows, fields] = await connection.query(sql, d);
 
         //console.log(rows);
         return rows;
-    }catch (err) {
+    } catch (err) {
         console.log(err);
-    }finally{
+    } finally {
         connection.end();
     }
 };
+
+/*
+getListNews = async function () {
+    return "実装中";
+};
+
+exports.getListNews = this.getListNews;*/
+
+exports.postTasks = postTasks;
