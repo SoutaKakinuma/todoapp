0075bae23ca5eb5d72ac009e16bb3e8313c31410
柿沼颯太 <s.kakinuma@g.dreamcareer.co.jp>
2022-04-18 18:58:36 +0900

    18日最後

--- src/tasks.js
+++ src/tasks.js
@@ -32,7 +32,7 @@ getTasks = async function () {
         connection = await mysql.createConnection(config.dbSetting);
         //SQL
         const sql =
-            "SELECT task_name, deadline, category_id FROM t_task";
+            "SELECT t_task.id, t_task.task_name, t_task.deadline, t_task.category_id, t_task.task_status FROM t_task;";
         const [rows, fields] = await connection.query(sql);
         return rows;
     } catch (err) {
@@ -45,11 +45,11 @@ getTasks = async function () {
 getTasksid = async function (id) {
     let connection = null;
     try {
-        connection = await mysql.createConnection(config.dbSetting)
+        connection = await mysql.createConnection(config.dbSetting);
         //SQL
         const sql =
-            "SELECT task_name, deadline, category_id FROM t_task";
-        let d = [id]    
+            "SELECT t_task.id, m_category.category_name, t_task.task_name, t_task.deadline, t_task.category_id, t_task.task_status, t_task.updated_at, t_task.created_at FROM t_task LEFT JOIN m_category ON t_task.category_id = m_category.id WHERE t_task.id = ?;";
+        let d = [id];    
         const [rows, fields] =await connection.query(sql, d);
         return rows;
     } catch (err) {
@@ -76,6 +76,25 @@ patchTasksid = async function (id, body) {
     }
 };
 
+deleteTasksid = async function (id) {
+    let connection = null;
+    try {
+        connection = await mysql.createConnection(config.dbSetting)
+        //SQL
+        const sql =
+            "DELETE FROM t_task WHERE id = ?;";
+        let d = [id];
+        const [rows, fields] = await connection.query(sql, d);
+        return rows;
+    } catch (err) {
+      console.log(err);
+    } finally {
+      connection.end();
+    }
+};
+
 exports.postTasks = postTasks;
 exports.getTasks = getTasks;
 exports.getTasksid = getTasksid;
+exports.patchTasksid = patchTasksid;
+exports.deleteTasksid = deleteTasksid;
